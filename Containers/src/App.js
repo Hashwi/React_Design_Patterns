d2b9411a7c7122574a12fdb2d36acf972aeb23bf
Hashwi <119130028+Hashwi@users.noreply.github.com>
2023-08-24 11:01:37 +0200

    dynamic loader

--- Containers/src/App.js
+++ Containers/src/App.js
@@ -1,17 +1,31 @@
-import { CurrentUserLoader } from "./CurrentUserLoader";
+// import { CurrentUserLoader } from "./CurrentUserLoader";
 import { UserInfo } from "./UserInfo";
-import { UserLoader } from "./UserLoader";
+// import { UserLoader } from "./UserLoader";
+// import { ProductLoader } from "./ProductLoader";
+import { ProductInfo } from "./ProductInfo";
+import { ResourceLoader } from "./ResourceLoader";
 
 function App() {
   return (
     <>
-      <CurrentUserLoader>
+      {/* <CurrentUserLoader>
         <UserInfo />
       </CurrentUserLoader>
 
       <UserLoader userId="456">
-            <UserInfo />
-        </UserLoader>
+        <UserInfo />
+      </UserLoader>
+
+      <ProductLoader productId="1234">
+        <ProductInfo />
+      </ProductLoader> */}
+
+      <ResourceLoader resourceUrl="./users/123" resourceName="user">
+        <UserInfo />
+      </ResourceLoader>
+      <ResourceLoader resourceUrl="./products/1234" resourceName="product">
+        <ProductInfo />
+      </ResourceLoader>
     </>
   );
 }
